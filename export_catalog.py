0a05fc120c0a641554de4c1c7eafa5ca1cac8ea7
Jun Su <junsu@jamstec.go.jp>
2024-11-08 09:36:05 +0900

    arrival information, correct azimuth

--- export_catalog.py
+++ export_catalog.py
@@ -1,15 +1,31 @@
 import os
 import glob
 import time
+import math
 import pickle
 import pandas as pd
-from picker import SeismicData
-from obspy import Catalog, UTCDateTime, read_events
+from picker import SeismicData, flatten_list
+from obspy import Catalog, UTCDateTime, read_events, read_inventory
 from obspy.core.event import Event as obspyEvent
-from obspy.core.event import Origin, Pick, Magnitude, CreationInfo, QuantityError, ResourceIdentifier
-from obspy.geodetics.base import gps2dist_azimuth
+from obspy.core.event import Origin, Pick, Arrival, Magnitude, CreationInfo, QuantityError, ResourceIdentifier
+from obspy.taup import TauPyModel
+from obspy.geodetics.base import gps2dist_azimuth, kilometers2degrees
 
-class GlobowCatalog(Catalog):
+model = TauPyModel(model="prem")
+prem_id = ResourceIdentifier(id=f"prem")
+# ak135_id = ResourceIdentifier(id=f"ak135")
+
+def get_station_dict(xml_path="rawdata_catalog_mass*_usta/*/*/stations/*.xml"):
+    inventory_dirs = glob.glob(xml_path)
+    station_dict = {}
+    for inventory_dir in inventory_dirs:
+        key = f"{inventory_dir.split('/')[-1].split('.')[0]}.{inventory_dir.split('/')[-1].split('.')[1]}"
+        if key in station_dict: continue
+        station = read_inventory(inventory_dir)[0][0]
+        station_dict[key] = {'latitude': station.latitude, 'longitude': station.longitude, 'elevation':station.elevation}
+    return station_dict
+
+class GlobalCatalog(Catalog):
     def update_catalog(self, datalist: SeismicData, result_csv_filenames: list, version: str) -> None:
         count = 0; length = len(result_csv_filenames)
         for result_csv in map(pd.read_csv, result_csv_filenames):
@@ -34,27 +50,39 @@ class GlobowCatalog(Catalog):
                     self.append(event)
 
                 azimuths = gps2dist_azimuth(
-                    lat1=row['station_lat'],
-                    lon1=row['station_lon'],
-                    lat2=event.preferred_origin().latitude,
-                    lon2=event.preferred_origin().longitude
+                    lat1=event.preferred_origin().latitude,
+                    lon1=event.preferred_origin().longitude,
+                    lat2=row['station_lat'],
+                    lon2=row['station_lon'],
                     )
                 
                 for phase in ['P', 'S']:
-                    pick_id = ResourceIdentifier(id=f"quakeml:jun.su/globowcat/{event_id}-{row['network']}_{row['station']}_LH-{phase}")
+                    pick_id = ResourceIdentifier(id=f"quakeml:jun.su/globocat/{event_id}-{str(row['network']).strip()}_{str(row['station']).strip()}_LH-{phase}")
                     if pick_id.get_referred_object() is None:
                         if pd.isna(row[f'{phase.lower()}_arrival_time']): continue
                         event.picks.append(Pick(
                             resource_id=pick_id,
                             time=row[f'{phase.lower()}_arrival_time'],
                             time_errors=QuantityError(confidence_level=row[f'{phase.lower()}_probability']),
-                            azimuth=azimuths[1],
                             backazimuth=azimuths[2],
-                            method=ResourceIdentifier(id="globowcat-eqt-v5"),
+                            method=ResourceIdentifier(id="globocat-eqt-v5"),
                             phase_hint=phase,
                             creation_info = CreationInfo(
                                 author="Jun Su", version=version, creation_time=UTCDateTime.now()
                                 ),
+                            ))
+                        distance = kilometers2degrees(azimuths[0]/1000)
+                        # print('distance:', distance)
+                        prem_arrival_time = model.get_travel_times(event.preferred_origin().depth, distance, [phase])
+                        if len(prem_arrival_time)>0:
+                            event.preferred_origin().arrivals.append(Arrival(
+                                resource_id=ResourceIdentifier(id=f"{pick_id.id}-PREM"),
+                                earth_model_id=prem_id,
+                                pick_id=pick_id,
+                                phase=phase,
+                                azimuth=azimuths[1],
+                                distance=distance,
+                                time_residual=(UTCDateTime(row[f'{phase.lower()}_arrival_time'])-event.preferred_origin().time)-prem_arrival_time[0].time,
                             ))
             count = count + 1
             print(f"Progress: [{count}/{length}] [{count/length*100:.1f}%] [{'='*int(count/length*20)}>{' '*(20-int(count/length*20))}]", end='\r')
@@ -63,7 +91,7 @@ class GlobowCatalog(Catalog):
 
 if __name__ == "__main__":
     old_version = "1.0-rc.1"
-    new_version = "1.0-rc.1"
+    new_version = "1.0"
     old_filename_suffix = ""
     new_filename_subfix = ""
     datalist_dir = "/Users/junsu/Documents/data_gcmt.pkl"
